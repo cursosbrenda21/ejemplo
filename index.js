7888f83cf7acd990f80556431436cd95e36be0f8
Brenda Yineth Gil <gyineth21@gmail.com>
2025-08-04 23:43:10 -0500

    change: files c4

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("HOLA");
 console.log("Cambio 2");
+console.log("Cambio 3");
+console.log("Cambio 4");
--- index2.js
+++ index2.js
@@ -1,2 +1,4 @@
 console.log("Hola mundo 2");
 console.log("Cambio  2");
+console.log("Cambio  3");
+console.log("Cambio  4");
